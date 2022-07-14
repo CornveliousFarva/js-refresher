8ce0a5c9acc0abcc1f3cade29359d3bdf2749e69
Greg Schuman <gcschuman@hotmail.com>
2022-07-14 13:08:08 -0700

    Added console logs for numbers and strings.

---
+++ console-log.js
@@ -0,0 +1,14 @@
+// Console log of a single number
+console.log(1)
+print ()
+// Console log of a single word
+console.log('Hubris')
+print()
+// Console log of a string (like a sentence)
+console.log('The third season of Solar Opposites was respectable.')
+// Math in JavaScript
+console.log(7 + 7);
+console.log(1561 - 282);
+console.log(69 / 9)
+console.log(3 * 5)
+// String Concatenation
