5ad2f849aae07410f1afe5114764995b50a17bdf
Greg Schuman <gcschuman@hotmail.com>
2022-07-18 13:22:30 -0700

    Added let.

--- console-log.js
+++ console-log.js
@@ -19,3 +19,23 @@ console.log('wild' + ' fire')
 // Length
 console.log('Scouse, not English'.length)
 console.log('      Fire Up Chips      '.trim())
+// Built-In Objects
+console.log(Math.random())
+console.log(Math.random(Math.floor) *50)
+// Variables- container for a value
+// Creating a variable-var
+// Var is a variable that cannot be reassigned
+const myName = 'Greg';
+console.log(myName);
+
+var favoriteFood = 'Subs';
+var numOfPieces = 2;
+var favoriteSubs = 'Jersey Mikes'
+console.log(favoriteFood);
+console.log(numOfPieces);
+console.log(favoriteSubs);
+// Creating a variable-let
+// Let means the variable can be reassigned a different value
+
+
+
